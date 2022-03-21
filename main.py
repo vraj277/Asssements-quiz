73e0e63b44c0d62c312cac92d5e5a1c00d0fc63b
Vraj Shah <>
2022-03-21 04:27:23 +0000

    welcome statment too long so added severals print statments

--- main.py
+++ main.py
@@ -1,1 +1,48 @@
-https://www.youtube.com/watch?v=cwJBEZjQJtc
+def main_menu():
+  print("|*******************************|")
+  print("| WELCOME TO THE MATH QUIZ 2022 |")
+  print("|*******************************|")
+  print("| Please select an option:      |")
+  print("|*                             *|")
+  print("| 1. Play Math quiz             |")
+  print("| 2. Know the rules             |")
+  print("| 3. Quit                       |")
+  print("|*******************************|")
+  choice = input("| Enter 1, 2, or 3: ")
+  
+  if choice == "1":
+    welcome_statment()
+  elif choice == "3":
+    print("Thanks for Playing :) !")
+    quit()
+  elif choice == "2":
+    print("| Here are 3 simple rules       |")
+    print("| 1. No cheating                |")
+    print("| 2. Dont use calculator only   |")
+    print("|    if necessary               |")
+    print("| 3. Have Fun!!                 |")
+
+main_menu()
+
+# welcome statment 
+def welcome_statment():
+  name = input("Please Enter Your name: ")
+  age  = int(input("Please Enter your age: ")
+  print("Hello, Welcome to My Math quiz 2022, 
+  print("please type in answer if the ai ask you to and") 
+  print("please read the rules, note that this is quiz is")
+  print("purely made for year 11 students so if you are")
+  print("from year 10 or below and/or above you") print("willeither find it difficult or easy, thanks and have fun")
+
+  
+
+             
+  
+
+
+
+
+# def play_MCQ_quiz():
+
+# Quiz Questions 
+#def get_MCQ_statements():
